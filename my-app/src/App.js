6a8f1ccb5d5bd4334c0c5c0f93d84d7ecdcd168d
Christopher Pineda <Christopher.Pineda59@myhunter.cuny.edu>
2022-09-08 18:27:42 -0400

    Added Create Account Page and route imports w/ styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+import { Routes, Route } from "react-router-dom";
+
+// Import your components here 
+import CreateAccount from './components/CreateAccount';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      {/* Add routes/your page components here  */}
+      <Route path="/CreateAccount" element={<CreateAccount />} />
+    </Routes>
   );
 }
 
 export default App;
+
