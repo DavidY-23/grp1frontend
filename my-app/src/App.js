60e4cdf5964f1d55706c6627a6df7d4acf4548ce
DavidY-23 <davidye0201@gmail.com>
2022-09-08 18:12:53 -0400

    Got the initial form with css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, renderMatches, Route, useNavigate} from 'react-router-dom';
 import { useState } from 'react';
+import {Link} from 'react-router-dom';
+import {Profile} from './component/Profile'
 
 function App() {
   const [name, setName] = useState("");
@@ -11,53 +13,55 @@ function App() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(age);
     console.log(name);
+    console.log(age);
     console.log(gender);
     console.log(weight);
     console.log(height);
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>Enter your name:
-        <input 
-          type="text" 
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </label>
-      <label>Enter your age:
-        <input 
-          type="text" 
-          value={age}
-          onChange={(e) => setAge(e.target.value)}
-        />
-      </label>
-      <label>Enter your gender:
-        <input 
-          type="text" 
-          value={gender}
-          onChange={(e) => setGender(e.target.value)}
-        />
-      </label>
-      <label>Enter your weight:
-        <input 
-          type="text" 
-          value={weight}
-          onChange={(e) => setWeight(e.target.value)}
-        />
-      </label>
-      <label>Enter your height:
-        <input 
-          type="text" 
-          value={height}
-          onChange={(e) => setHeight(e.target.value)}
-        />
-      </label>
-      <input type="submit" />
-    </form>
-  )
+    <div>
+      <form className="form-part" onSubmit={handleSubmit}>
+<label>Enter your name:
+  <input 
+    type="text" 
+    value={name}
+    onChange={(e) => setName(e.target.value)}
+  />
+</label>
+<label>Enter your age:
+  <input 
+    type="text" 
+    value={age}
+    onChange={(e) => setAge(e.target.value)}
+  />
+</label>
+<label>Enter your gender:
+  <input 
+    type="text" 
+    value={gender}
+    onChange={(e) => setGender(e.target.value)}
+  />
+</label>
+<label>Enter your weight:
+  <input 
+    type="text" 
+    value={weight}
+    onChange={(e) => setWeight(e.target.value)}
+  />
+</label>
+<label>Enter your height:
+  <input 
+    type="text" 
+    value={height}
+    onChange={(e) => setHeight(e.target.value)}
+  />
+</label>
+<input type="submit" name="Submit"  />
+</form>
+    </div>
+    )
 }
 
 export default App;
