cd17e6dc5215c2463d4bf9a8893bd5c2051434c1
DavidY-23 <davidye0201@gmail.com>
2022-09-08 22:55:03 -0400

    testing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,8 @@
 import './App.css';
-import {BrowserRouter as Router, renderMatches, Route, useNavigate} from 'react-router-dom';
 import { useState } from 'react';
 import {Link} from 'react-router-dom';
 import {Profile} from './component/Profile'
-
+import { Routes, Route } from "react-router-dom";
 function App() {
   const [name, setName] = useState("");
   const [age, setAge] = useState("");
@@ -21,47 +20,11 @@ function App() {
   }
 
   return (
-    <div>
-      <form className="form-part" onSubmit={handleSubmit}>
-<label>Enter your name:
-  <input 
-    type="text" 
-    value={name}
-    onChange={(e) => setName(e.target.value)}
-  />
-</label>
-<label>Enter your age:
-  <input 
-    type="text" 
-    value={age}
-    onChange={(e) => setAge(e.target.value)}
-  />
-</label>
-<label>Enter your gender:
-  <input 
-    type="text" 
-    value={gender}
-    onChange={(e) => setGender(e.target.value)}
-  />
-</label>
-<label>Enter your weight:
-  <input 
-    type="text" 
-    value={weight}
-    onChange={(e) => setWeight(e.target.value)}
-  />
-</label>
-<label>Enter your height:
-  <input 
-    type="text" 
-    value={height}
-    onChange={(e) => setHeight(e.target.value)}
-  />
-</label>
-<input type="submit" name="Submit"  />
-</form>
-    </div>
-    )
+    <Routes>
+      {/* Add routes/your page components here  */}
+      <Route path="/Profile" element={<Profile />} />
+    </Routes>
+  )
 }
 
 export default App;
