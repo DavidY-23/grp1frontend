e836fcc5bd8d32e60cdca1dfe9174613f42eb6ae
Calvin <calvin.wang161@myhunter.cuny.edu>
2022-09-12 12:53:36 -0400

    Added Login Page with preliminary front end Google Auth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { render } from '@testing-library/react';
 import React, { Component } from 'react';
 import { Routes, Route } from "react-router-dom";
+import LoginPage from './components/LoginPage';
 
 // Import your components here 
 import CreateAccount from './components/CreateAccount';
 
 
 function App() {
+  /* global google */
   return (
     <div>
       <p>Main page to be implemented</p>
@@ -14,6 +16,7 @@ function App() {
       {/* Add routes/your page components here  */}
       <Route path="/CreateAccount" element={<CreateAccount />} />
       
+      <Route path="/LoginPage" element={<LoginPage/>}/>
     </Routes>
     </div>
 
