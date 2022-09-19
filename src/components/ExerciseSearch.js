1c2b7ee50540885dfb756de7fe968925286e6433
Hardip Chander <hardipchander@gmail.com>
2022-09-18 21:38:09 -0400

    Excersie Page is 90 percent done

--- src/components/ExerciseSearch.js
+++ src/components/ExerciseSearch.js
@@ -2,14 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'
 import "./styles/ExerciseSearch.css"
 import { Routes, Route } from "react-router-dom";
+import armsPic from '../images/Arms.png';
+import legsPic from '../images/legs.png';
 
 function ExceriseSearch() {
     return (
         <div id="wholeComponet">
-            <div>
+            <div id="surrounding">
                 <form action="https:/httpbin.org/get" method="get">
                     <input id="SearchBar" type="text" placeholder="Search for exercises best suited for you!"></input> 
                 </form>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+            </div>
+            <div id="images">
+                <div class="pad">
+                    <img src={armsPic} height="150" width="150"/>
+                    <a href="#">Arms</a>
+                </div>
+                <div class="pad">
+                    <img src={legsPic} height="150" width="150"/>
+                    <a href="#">Legs</a>
+                </div>
+                <div class="pad">
+                    <a href="#">And Much More</a>
+                </div>
+
             </div>
         </div>
     );
