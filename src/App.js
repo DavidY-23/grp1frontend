fb70f8d2bda68a6eb9ded564aee7ffd804ab38cf
ccpineda76 <Christopher.Pineda59@myhunter.cuny.edu>
2022-09-24 13:20:35 -0400

    fixed importing and routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -11,27 +11,22 @@ import WelcomePage from "./components/WelcomePage";
 import ContactPage from "./components/Contact";
 import AboutPage from "./components/About";
 import HomePage from "./components/HomePage";
-import Profile from './components/Profile';
-import ExerciseSearch from './components/ExerciseSearch.js';
-import RecipeSearch from "./components/RecipeSearch.js";
+
 
 function App() {
   /* global google */
   return (
     <div>
-        <Routes>
-          {/* Add routes/your page components here  */}
-          <Route path="/home/:state" element={<HomePage />} />
-          <Route exact path="/" element={<WelcomePage />} />
-          <Route exact path="/About" element={<AboutPage />} />
-          <Route exact path="/Contact" element={<ContactPage />} />
-          <Route path="/CreateAccount" element={<CreateAccount />} />
-          <Route path="/FirstTimeLogin" element={<FirstTimeLogin />} />
-          <Route path="/LoginPage" element={<LoginPage />} />
-          <Route path="/Profile" element={<Profile />} />
-          <Route path="/ExerciseSearch" element={<ExerciseSearch/>}/>
-          <Route path="/RecipeSearch" element={<RecipeSearch/>}/>
-        </Routes>
+      <Routes>
+        {/* Add routes/your page components here  */}
+        <Route path="/home/:state" element={<HomePage />} />
+        <Route exact path="/" element={<WelcomePage />} />
+        <Route exact path="/About" element={<AboutPage />} />
+        <Route exact path="/Contact" element={<ContactPage />} />
+        <Route path="/CreateAccount" element={<CreateAccount />} />
+        <Route path="/FirstTimeLogin" element={<FirstTimeLogin />} />
+        <Route path="/LoginPage" element={<LoginPage />} />
+      </Routes>
     </div>
   );
 }
