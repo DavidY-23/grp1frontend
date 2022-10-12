3b63f45479635d1d82e425891996b836eaa01a59
Hardip Chander <hardipchander@gmail.com>
2022-10-12 00:05:35 -0400

    Made the pages more responsive

--- src/components/MentalHealth.js
+++ src/components/MentalHealth.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import "./styles/MentalHealth.css"
 import mentalHealth from "../images/MentalHealth.jpg"
+import notebook from "../images/notebook.png";
 
 function MentalHealth() {
 
@@ -16,29 +17,29 @@ function MentalHealth() {
                     <img src={mentalHealth} className="MentalImage"/>
                 </div>
             </header>
-            <section class="MentalBoxes">
-                <div class="MentalContainer">
-                    <div class="MentalBox">
-                        <h2 className='Theh1sh2s'>10 Tips</h2>
-                        <p>
-                            <a href="http://www.bcmhsus.ca/about/news-stories/stories/10-tips-to-boost-your-mental-health" target="_blank">Link</a>
-                        </p>
-                    </div>
-                    <div class="MentalBox">
-                        <h2 className='Theh1sh2s'>Option 2</h2>
-                        <p>
-                            Have an option here.
-                        </p>
-                    </div>
-                    <div class="MentalBox">
-                        <h2 className='Theh1sh2s'>Journal Entry</h2>
-                        <p>
-                            <Link to="/JournalEntry" id="JumpToJournal">
-                                Write a entry.
-                            </Link>
-                        </p>
-                    </div>
+            <section className="MentalBoxes">
+                <div className="MentalBox">
+                    <h2 className='Theh1sh2s'>Tips</h2>
+                    <p>
+                        <a href="http://www.bcmhsus.ca/about/news-stories/stories/10-tips-to-boost-your-mental-health" target="_blank">Ten Tips</a>
+                    </p>
                 </div>
+                <div className="MentalBox">
+                    <h2 className='Theh1sh2s'>Option 2</h2>
+                    <p>
+                        Have an option here.
+                    </p>
+                </div>
+                <div className="MentalBox" id="journalbox">
+                    <p>
+                        <Link to="/JournalEntry" id="JumpToJournal">
+                            <h3 id="JournalTitle">Write a entry.</h3>
+                        </Link>
+                    </p>
+                    <img src={notebook}/>
+                    
+                </div>
+                
             </section>
         </div>
     );
